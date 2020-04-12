126c66742ac9f0f742da73784e72185755919232
Karel <Karel4ever@gmail.com>
2020-04-12 17:17:43 +0200

    Added login and signup functionality

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Firebase Chat',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primaryColor: Colors.white,
+        primaryColor: Colors.blue,
       ),
       home: StreamBuilder<FirebaseUser>(
         stream: Provider.of<AuthService>(context, listen: false).user,
